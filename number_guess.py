46046775a982854981c37a02707aaf113ce81565
John Adams Jr <john.c.adams32@gmail.com>
2021-12-08 21:34:27 -0700

    5 todo's left. Rearranged code and took out redundant code

--- number_guess.py
+++ number_guess.py
@@ -1,26 +1,29 @@
 import random
 from logo import logo
 
-numset = [num for num in range(1,101)]
 
 print("Welcome to Number Guess!")
 print(logo)
 
+numset = [num for num in range(1,101)]
+computer_number = random.choice(numset)
+
 difficulty = input('Would you like to play on Easy, Medium, or Hard? Type "E", "M" or "H"\n').lower()
-easy_lives = 10
-medium_lives = 7
-hard_lives = 5
+
 if difficulty == "e":
-    print(f"Guess a number from 1 to 100, you have {easy_lives} lives.")
+    lives = 10
+    first_guess = int(input(f"Guess a number from 1 to 100, you have {lives} lives.\n"))
 elif difficulty == "m":
-    print(f"Guess a number from 1 to 100, you have {medium_lives} lives.")
+    lives = 7
+    first_guess = int(input(f"Guess a number from 1 to 100, you have {lives} lives.\n"))
 else:
-    print(f"Guess a number from 1 to 100, you have {hard_lives} lives.")
+    lives = 5
+    first_guess = int(input(f"Guess a number from 1 to 100, you have {lives} lives.\n"))
+
 
 # Setting Up structure
 
 # Tasklist:
-# pick a number from a list of 1 to 100 for the user to guess
 # ask the user to input their guess and remove a life
 # check if the guess matches and report either high or low to the user
 # if the user guesses correctly print you win

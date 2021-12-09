91bc1902c882c18b69a3b022889c27c0cbbc55ae
John Adams Jr <john.c.adams32@gmail.com>
2021-12-08 20:39:51 -0700

    Inital project outline

---
+++ number_guess.py
@@ -0,0 +1,13 @@
+# Setting Up structure
+
+Tasklist:
+Welcome the user and display logo
+import any modules needed
+Get the users input for easy(10 lives), medium(7 lives), and hard(5 lives) levels
+Notify user of guessing dataset
+pick a number from a list of 1 to 100 for the user to guess
+ask the user to input their guess and remove a life
+check if the guess matches and report either high or low to the user
+if the user guesses correctly print you win
+ask the user if they would like to play again
+if yes repeat program, if no exit.

4d64d15b351be96d90db01af99a4bf4df33fe5e0
John Adams Jr <john.c.adams32@gmail.com>
2021-12-08 21:06:30 -0700

    6 todos left on the task list

--- number_guess.py
+++ number_guess.py
@@ -1,10 +1,25 @@
+import random
+from logo import logo
+
+numset = [num for num in range(1,101)]
+
+print("Welcome to Number Guess!")
+print(logo)
+
+difficulty = input('Would you like to play on Easy, Medium, or Hard? Type "E", "M" or "H"\n').lower()
+easy_lives = 10
+medium_lives = 7
+hard_lives = 5
+if difficulty == "e":
+    print(f"Guess a number from 1 to 100, you have {easy_lives} lives.")
+elif difficulty == "m":
+    print(f"Guess a number from 1 to 100, you have {medium_lives} lives.")
+else:
+    print(f"Guess a number from 1 to 100, you have {hard_lives} lives.")
+
 # Setting Up structure
 
 # Tasklist:
-# Welcome the user and display logo
-# import any modules needed
-# Get the users input for easy(10 lives), medium(7 lives), and hard(5 lives) levels
-# Notify user of guessing dataset
 # pick a number from a list of 1 to 100 for the user to guess
 # ask the user to input their guess and remove a life
 # check if the guess matches and report either high or low to the user
